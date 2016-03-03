38c5491bdedaa3bf9563ae11429313b74dfa3525
psapin <psapin@users.noreply.github.com>
2016-03-02 19:56:26 -0600

    fixed, modularized tests

--- test/testPrefs.js
+++ test/testPrefs.js
@@ -13,5 +13,11 @@ QUnit.test("Did Preferences Panel Open:", function (assert) {
 QUnit.test("Did Buttons Change to Black/White:", function (assert) {
     changeButtonTheme(blackWhiteTheme);
     var buttonBgColor = $('#createGame').css('background-color');
-    assert.equal(buttonBgColor, 'black');
+    assert.equal(buttonBgColor, 'rgb(0, 0, 0)');
+});
+
+QUnit.test("Did Buttons Change to Blue/Orange:", function (assert) {
+    changeButtonTheme(blueOrangeTheme);
+    var buttonBgColor = $('#createGame').css('background-color');
+    assert.equal(buttonBgColor, 'rgb(255, 165, 0)');
 });
