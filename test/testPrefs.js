93056e0a5383513eabffdff3e1ad4402e3a0bbbd
Ran Crook <racrook2@illinois.edu>
2016-03-17 16:27:14 -0500

    tests

--- test/testPrefs.js
+++ test/testPrefs.js
@@ -14,14 +14,52 @@ QUnit.test("Did Preferences Modal Open:", function (assert) {
     assert.equal(modalDisplay, 'block');
 });
 
-QUnit.test("Did Buttons Change to Black/White:", function (assert) {
-    changeButtonTheme(blackWhiteTheme);
+QUnit.test("Did Theme Change to Purple:", function (assert) {
+    changeButtonTheme(purpleTheme);
     var buttonBgColor = $('#createGame').css('background-color');
-    assert.equal(buttonBgColor, 'rgb(0, 0, 0)');
+    assert.equal(buttonBgColor, 'rgb(102, 0, 102)');
+	var bgColor = $('body').css('background-color');
+	assert.equal(bgColor, 'rgb(153, 51, 153)');
 });
 
-QUnit.test("Did Buttons Change to Blue/Orange:", function (assert) {
-    changeButtonTheme(blueOrangeTheme);
+QUnit.test("Did Theme Change to Orange:", function (assert) {
+    changeButtonTheme(orangeTheme);
     var buttonBgColor = $('#createGame').css('background-color');
-    assert.equal(buttonBgColor, 'rgb(255, 165, 0)');
+    assert.equal(buttonBgColor, 'rgb(255, 102, 0)');
+	var bgColor = $('body').css('background-color');
+	assert.equal(bgColor, 'rgb(255, 165, 0)');
+});
+
+QUnit.test("Did Theme Change to Black:", function (assert) {
+    changeButtonTheme(blackTheme);
+    var buttonBgColor = $('#createGame').css('background-color');
+    assert.equal(buttonBgColor, 'rgb(0, 0, 0)');
+	var bgColor = $('body').css('background-color');
+	assert.equal(bgColor, 'rgb(31, 31, 31)');
+});
+
+QUnit.test("Did Background Change to Charizard:", function (assert) {
+    changeBackground('bg1');
+    var bgImage = $('#main').css('background-image');
+	bgImage = bgImage.slice(-9, -2);
+    assert.equal(bgImage, "bg1.png");
+});
+
+QUnit.test("Did Background Change to Greninja:", function (assert) {
+    changeBackground('bg2');
+    var bgImage = $('#main').css('background-image');
+	bgImage = bgImage.slice(-9, -2);
+    assert.equal(bgImage, "bg2.png");
+});
+
+QUnit.test("Did Name Not Change when Empty:", function (assert) {
+    changeName("");
+	var name = $('#greeting').html();
+	assert.equal(name, 'Hello, guest');
+});
+
+QUnit.test("Did Name Change when Not Empty:", function (assert) {
+    changeName("Ran");
+	var name = $('#greeting').html();
+	assert.equal(name, 'Hello, Ran');
 });
