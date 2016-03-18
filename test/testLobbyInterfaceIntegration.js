edc3d7c9c2348fe649f3efa044462a2650902f9a
Joel <joel.einbinder@gmail.com>
2016-03-17 20:02:08 -0500

    Test things

--- test/testLobbyInterfaceIntegration.js
+++ test/testLobbyInterfaceIntegration.js
@@ -1,9 +1,24 @@
-var Multiplayer;
-
-socket = socket.socket.connect("ctw.firecaster.com:80");
-
 QUnit.test("Test startGameCheck true", function(assert) {
-    Multiplayer.players = [1,2];
-    Multiplayer.readyPlayers = 2;
-    assert.ok("No one has pressed ready");   
+   Multiplayer.createGame();
+   var done = assert.async();
+    setTimeout(function() {
+      done();
+    },1000);
+   assert.ok(Multiplayer.startGameCheck()==false, "No one is ready");
+   Multiplayer.readyGame();
+   var done1 = assert.async();
+   setTimeout(function() {
+    done1();
+   },5000);
+   assert.equal(Multiplayer.readyPlayers, 1);
+   console.log("Checked Ready Players first time");
+   assert.ok(Multiplayer.startGameCheck()==false, "One person is ready");
+   Multiplayer.socket.emit("shout",{type:"ready"});
+   var done2 = assert.async();
+   setTimeout(function() {
+    done2();
+    }, 5000);
+   assert.equal(Multiplayer.readyPlayers, 2);
+   assert.ok(Multiplayer.startGameCheck(), "Two people are ready");
+   
 });
