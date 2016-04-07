2adfb223afb7b5c07c9bc3d3a3f8a82cb915fc7d
psapin <psapin@users.noreply.github.com>
2016-04-07 16:41:43 -0500

    Test rewrite

--- test/testGameSettings.js
+++ test/testGameSettings.js
@@ -1,3 +1,13 @@
+QUnit.config.autostart = false;
+var socket;
+var GAME_TYPE = 268201;
+Multiplayer.createGame();
+Multiplayer.startGame();
+setTimeout(function() {
+    QUnit.start();
+}, 1000);
+
+
 QUnit.module("Game Settings Test", {
     beforeEach: function () {
         /*var done = assert.async();
@@ -30,70 +40,11 @@ QUnit.test("Did Game Settings Modal Close:", function (assert) {
     assert.equal(modalDisplay, 'none');
 });
 
+QUnit.test("Did Default Settings Apply When Modal Closed:", function (assert) {
 
-QUnit.test("Did Theme Change to Orange:", function (assert) {
-    changeButtonTheme(orangeTheme);
-    var buttonBgColor = $('#createGame').css('background-color');
-    assert.equal(buttonBgColor, 'rgb(255, 102, 0)');
-    var bgColor = $('body').css('background-color');
-    assert.equal(bgColor, 'rgb(255, 165, 0)');
-});
+}); 
 
-QUnit.test("Did Theme Change to Black:", function (assert) {
-    changeButtonTheme(blackTheme);
-    var buttonBgColor = $('#createGame').css('background-color');
-    assert.equal(buttonBgColor, 'rgb(0, 0, 0)');
-    var bgColor = $('body').css('background-color');
-    assert.equal(bgColor, 'rgb(31, 31, 31)');
-});
+QUnit.test("Did Game Settings Apply on Form Submission:", function (assert) {
 
+}); 
 
-QUnit.test("Did Background change to Red:", function (assert) {
-    changeBackgroundColor('red');
-    var bgColor = $('body').css('background-color');
-    assert.equal(bgColor, 'rgb(255, 0, 0)');
-});
-
-QUnit.test("Did Name change to TestName:", function (assert) {
-    changeName('TestName');
-    var user = db.getValue('Username');
-    assert.equal(user, 'TestName');
-});
-
-QUnit.test("Did Font Size change to Small:", function (assert) {
-    changeFontSize('small');
-    var fontsize = $('#buttons').css('font-size');
-    assert.equal(fontsize, '13px');
-});
-QUnit.test("Did Font Size change to Large:", function (assert) {
-    changeFontSize('x-large');
-    var fontsize = $('#buttons').css('font-size');
-    assert.equal(fontsize, '24px');
-});
-
-
-QUnit.test("Did Background Change to Charizard:", function (assert) {
-    changeBackground('bg1');
-    var bgImage = $('#main').css('background-image');
-    bgImage = bgImage.slice(-9, -2);
-    assert.equal(bgImage, "bg1.png");
-});
-
-QUnit.test("Did Background Change to Greninja:", function (assert) {
-    changeBackground('bg2');
-    var bgImage = $('#main').css('background-image');
-    bgImage = bgImage.slice(-9, -2);
-    assert.equal(bgImage, "bg2.png");
-});
-
-QUnit.test("Did Name Not Change when Empty:", function (assert) {
-    changeName("");
-    var name = $('#greeting').html();
-    assert.equal(name, 'Hello, guest');
-});
-
-QUnit.test("Did Name Change when Not Empty:", function (assert) {
-    changeName("Ran");
-    var name = $('#greeting').html();
-    assert.equal(name, 'Hello, Ran');
-});
