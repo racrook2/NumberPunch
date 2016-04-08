1b1c8438b7535bf145252917bdd6abda7ca21b78
psapin <psapin@users.noreply.github.com>
2016-04-07 19:47:17 -0500

    added more gamesettings tests

--- test/testGameSettings.js
+++ test/testGameSettings.js
@@ -2,8 +2,8 @@
 QUnit.config.autostart = false;
 var socket;
 var GAME_TYPE = 268201;
+
 Multiplayer.createGame();
-//Multiplayer.startGame();
 setTimeout(function() {
     QUnit.start();
 }, 1000);
@@ -12,12 +12,6 @@ var modalID = "#gamesettings";
 
 QUnit.module("Game Settings Test", {
     beforeEach: function () {
-
-        /*var done = assert.async();
-        $('#qunit-fixture').load('../index.html');
-        $('document').ready(function() {
-            done();
-        })*/
     },
     afterEach: function () {
     }
@@ -38,15 +32,36 @@ QUnit.test("Did Default Settings Apply When Modal Closed:", function (assert) {
     assert.equal(GameInstance.getGameRule(), 0);
 }); 
 
-QUnit.test("Did Game Settings Apply on Form Submission:", function (assert) {
+QUnit.test("Did Modified Penalty Settings Apply on Form Submission:", function (assert) {
     $('input[name=penalties]').val('2');
+    assert.equal($('input[name=penalties]').val(), 2);
+    $('#gamesettingsForm').submit();
+    setTimeout(function() {
+        assert.equal(GameInstance.getPenaltyThreshold(), 2);
+    }, 1000);
+}); 
+
+QUnit.test("Did Modified Game Type Settings Apply on Form Submission:", function (assert) {
     $('input[name=op]')[1].checked = true;
     assert.equal($('input[name=op]:radio:checked').val(), 'mult');
     $('#gamesettingsForm').submit();
     setTimeout(function() {
-        assert.equal(GameInstance.getPenaltyThreshold(), 2);
         assert.equal(GameInstance.getGameRule(), 1);
     }, 1000);
-    
 }); 
 
+QUnit.test("Did Game Settings Modal Close on Form Submission:", function (assert) {
+    
+    var modalDisplay = $(modalID).css('display');
+    if (modalDisplay == 'none') {
+        openModal(modalID);
+    }
+    modalDisplay = $(modalID).css('display');
+    assert.equal(modalDisplay, 'block');
+    $('#gamesettingsForm').submit();
+    setTimeout(function() {
+        modalDisplay = $(modalID).css('display');
+        assert.equal(modalDisplay, 'none');
+    }, 2000);
+    
+});
